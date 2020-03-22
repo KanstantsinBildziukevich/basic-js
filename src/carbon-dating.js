7841539390a460c07a5b251a124818cddbf70e2b
Constantine <131313Constantine@gmail.com>
2020-03-22 10:24:44 +0300

    feat: carbon-task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,15 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  let val;
+  if (typeof sampleActivity == 'string')
+  {
+    val = parseFloat(sampleActivity);
+  }
+  else return false;
+
+  if (val > 0 && val < MODERN_ACTIVITY){
+    return Math.ceil(Math.log(MODERN_ACTIVITY / val) /(0.693 / HALF_LIFE_PERIOD))
+  }
+  else return false;
 };
